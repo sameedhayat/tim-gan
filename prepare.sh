1fc62f6068c40be17568b0354549379bdc7ee889
Sameed Hayat <sameedhayat@gmail.com>
2023-02-27 16:19:12 +0100

    Add prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,7 @@
+pip install -r requirements.txt
+
+wget https://storage.googleapis.com/tim-gan-paper/checkpoints.tar
+tar -xvf checkpoints.tar -C ../
+
+wget https://storage.googleapis.com/tim-gan-paper/processed_data.tar
+tar -xvf processed_data.tar
